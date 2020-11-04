6f7c68dc49d16e3cbb7aadfffdc781a2c726325d
BartlomiejF <bartlomiej.fliszkiewicz@gmail.com>
2020-11-04 10:47:14 +0100

    changes

--- temp_humid_sensor.py
+++ temp_humid_sensor.py
@@ -1,28 +1,39 @@
 import requests
 import adafruit_dht
 import board
+import digitalio
 import time
 
 """
 This is temperature and humidity measurement script. Be sure to use proper GPIO
-pin or change it in the script.
+pin or change it in the script. DHT11 seemed to sometimes fail to read
+temperature or humidity so the script tries 20 times to read sensor data.
 """
 
 if __name__ == "__main__":
-    try:
-        sensor = adafruit_dht.DHT11(board.D7)  # The GPIO pin is here
+    # change pin in two below lines:
+    digitalio.DigitalInOut(board.D27).direction = digitalio.Direction.INPUT
+    sensor = adafruit_dht.DHT11(board.D27)
 
-        humid, temp = sensor.humidity, sensor.temperature
-
+    temp = None
+    humid = None
+    for i in range(20):
+        try:
+            if not humid:
+                humid = sensor.humidity
+            if not temp:
+                temp = sensor.temperature
+            print(f"{i}. {time.ctime()}           Temperature: {temp:.1f} Humidity: {humid:.1f}%")
+            break
+        except RuntimeError:
+            print(f"{i}. {time.ctime()}          RuntimeError")
+            time.sleep(1)
+        except:
+            print(f"{i}. {time.ctime()}          sth went wrong")
+            time.sleep(1)
+    if temp and humid:
         r = requests.post(
-            "http://127.0.0.1:5000//TempHumidMonitor",
-            params={
-                "temperature": f"{temp:.1f}",
-                "humidity": f"{humid:.1f}"
-                },
+            "http://127.0.0.1:5000/TempHumidMonitor",
+            params={"temperature": f"{temp:.1f}",
+            "humidity": f"{humid:.1f}"},
             )
-
-        print(f"{time.ctime()}           Temperature: {temp:.1f} Humidity: {humid:.1f}%")
-
-    except:
-        print(f"{time.ctime()}           sth went wrong")
